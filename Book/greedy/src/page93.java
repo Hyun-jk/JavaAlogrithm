28eff02c6bbf80a6900f82166abd175d09ddfb39
kim jaehyun <jaehyoen3@gmail.com>
2023-02-07 17:56:56 +0900

    page93

---
+++ Book/greedy/src/page93.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class page93 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); //배열의 수
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i =0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        int first = arr[n - 1];
+        int second = arr[n - 2];
+        int result = 0;
+
+        while (m != 0){
+            for(int i =0; i<k; i++){
+                if (m == 0){
+                    break;
+                }
+                result += first;
+                m -= 1;
+            }
+            result += second;
+            m -= 1;
+        }
+        System.out.println(result);
+
+    }
+}
