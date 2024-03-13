cfb643c6a8fcf8976e16e32955c1b5382db04f97
hyun <jaehyoen3@gmail.com>
2024-03-13 21:29:43 +0900

    String(문자열) - 03.문장 속 긴 단어 찾기

---
+++ WebSIte/Lecture/String_03.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class String_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String temp = sc.nextLine();
+        String answer = "";
+        String[] arr = temp.split(" ");
+        int maxLen = Integer.MIN_VALUE,pos;
+
+        for(String x : arr){
+            if (x.length() > maxLen) {
+                maxLen = x.length();
+                answer = x;
+            }
+        }
+        //indexOf + subString으로 푸는 방법 다시 해보기.
+        System.out.println("answer = " + answer);
+        
+    }
+}
