d41db82ad997dd9921594e82faf65d66a9336d45
hyun <jaehyoen3@gmail.com>
2024-03-21 00:22:01 +0900

    Sling Window - 최대 매출 구하기

---
+++ WebSIte/Lecture/twoPointers_SlidingWindow/SlidingWindow_01.java
@@ -0,0 +1,42 @@
+package twoPointers_SlidingWindow;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SlidingWindow_01 {
+    public int solution(int n, int k, int[] arr){
+        int answer = 0;
+        int temp = 0;
+
+        for (int i = 0; i < k; i++) {
+            temp += arr[i];
+        }
+        for (int j = k; j < arr.length; j++) {
+            temp += + arr[j] - arr[j-k];
+            answer = Math.max(answer,temp);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+        
+        
+        SlidingWindow_01 sw01 = new SlidingWindow_01();
+        System.out.println("answer = " + sw01.solution(n,k,arr));
+
+
+
+    }
+}
