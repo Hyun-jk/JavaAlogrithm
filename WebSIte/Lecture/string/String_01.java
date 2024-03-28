242716eab6d63344f723fa748b38f3a076cfe6b3
hyun <jaehyoen3@gmail.com>
2024-03-28 14:46:09 +0900

    String - 1.문자찾기

---
+++ WebSIte/Lecture/string/String_01.java
@@ -0,0 +1,20 @@
+package string;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class String_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next().toLowerCase();
+        char c = sc.next().charAt(0);
+
+        int answer = 0;
+
+        for (char x : str.toCharArray()) {
+            if(x == c) answer++;
+        }
+        System.out.println("answer = " + answer);
+        
+    }
+}
